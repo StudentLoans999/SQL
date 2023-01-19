f6a29bf2cd0df506a8fb9c954733bf6ba2e4dc92
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-19 13:29:25 -0500

    Update RunningTasks.sql

--- RunningTasks.sql
+++ RunningTasks.sql
@@ -1,3 +1,5 @@
+-- Outputs currently running tasks whose status is less than 2, with some columns about the Task and Scheduling information
+
 SELECT t.task_name, si.start_time, si.end_time, a.task_id, a.task_process_time, a.task_status
 FROM ABCLog.AUTOMATION AS a
 INNER JOIN ABCMain.TASK AS t
