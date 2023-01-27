acdf7c0e55171a3461f9ff3fd429740c58c2688c
David Richey <davidsacc@gmail.com>
2023-01-27 14:54:30 -0500

    Delete KillXRunningTask.sql

--- KillXRunningTask.sql
+++
@@ -1,6 +0,0 @@
--- Kills currently running Tasks whose status is equal to 2, by inputting the Task ID
-
-UPDATE ABCLog.AUTOMATION
-SET task_status = 3
-WHERE task_status = 2
-  AND task_id IN (X) -- Replace the "X" with the task_id you want to kill (it'll be a number)
